356a1ad6b6919cee7c19550d76d8c76199f40c6f
JCM8023 <738238912@qq.com>
2018-11-25 14:15:12 +0800

    //test

---
+++ src/main/java/com/example/Test.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by Administrator on 2018/11/25.
+ */
+public class Test {
+
+    public static void main(String[] arr){
+        System.out.println("heheh");
+    }
+}
